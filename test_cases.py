0911321a13913e05defb5a8cb038238b0fa8c729
Thomas Turner <thomasturner2003@outlook.com>
2026-01-29 13:57:59 +0000

    fixed error with twist relative angles

--- test_cases.py
+++ test_cases.py
@@ -33,28 +33,27 @@ def json_case_generator(file_path):
         yield case
         
 
-file_path = 'Dataset/long_long_triplets.json'
+file_paths = ['Dataset/long_long_triplets.json', 'Dataset/short_long_triplets.json', 'Dataset/short_short_triplets.json']
 solver = dsmiller.Solver("oriented")
 ito = dsmiller.Ito()
 diameter = 10E-3
 flow = dsmiller.Flow(5,998,1E-3,diameter)
 errors = []
 elbows_errors = []
-for case in json_case_generator(file_path):
-    bends = [] 
-    for r,o in zip(case['bend_radii'], case["twists"]):
-        bends.append(dsmiller.Bend(int(r/diameter),o,ito.get_k(r/diameter,flow.reynolds)))
-    pipes = []
-    pipes.append(dsmiller.PipeSection(case['inlet_length']/diameter))
-    for l in case['connector_lengths']:
-        pipes.append(dsmiller.PipeSection(l/diameter))
-    pipes.append(dsmiller.PipeSection(case['outlet_length']/diameter))
-    errors.append((case['pressure_drop'] - solver.get_pressure_drop(bends, pipes, flow)[0]) / case['pressure_drop'])
-    outlet_inlet_dp = dsmiller.blasius_darcy(case['inlet_length']+ case['outlet_length'], flow)
-    elbows_errors.append((case['pressure_drop'] - solver.get_pressure_drop(bends, pipes, flow)[0]) / (case['pressure_drop']-outlet_inlet_dp))
-print(errors)
+for file_path in file_paths:
+    for case in json_case_generator(file_path):
+        bends = [] 
+        for r,o in zip(case['bend_radii'], case["twists"]):
+            bends.append(dsmiller.Bend(int(r/diameter),o,ito.get_k(r/diameter,flow.reynolds)))
+        pipes = []
+        pipes.append(dsmiller.PipeSection(case['inlet_length']/diameter))
+        for l in case['connector_lengths']:
+            pipes.append(dsmiller.PipeSection(l/diameter))
+        pipes.append(dsmiller.PipeSection(case['outlet_length']/diameter))
+        errors.append((case['pressure_drop'] - solver.get_pressure_drop(bends, pipes, flow)[0]) / case['pressure_drop'])
+        outlet_inlet_dp = dsmiller.blasius_darcy(case['inlet_length']+ case['outlet_length'], flow)
+        elbows_errors.append((case['pressure_drop'] - solver.get_pressure_drop(bends, pipes, flow)[0]) / (case['pressure_drop']-outlet_inlet_dp))
 print(f"{100*np.mean(np.abs(errors)):.2f}%")
 print("")
-print(errors)
 print(f"{100*np.mean(np.abs(elbows_errors)):.2f}%")
 print("")
